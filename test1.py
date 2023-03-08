c000ad70288f9360e182efbf9fdf78040d995229
Zi Zhang <z785zhan@uwaterloo.ca>
2023-03-08 15:08:25 -0500

    wiring test file

---
+++ test1.py
@@ -0,0 +1,19 @@
+import time
+from gpiozero import LED, LightSensor
+
+led = LED(4)
+led.on()
+print("LED on!")
+time.sleep(1)
+led.off()
+print("LED off!")
+
+lsr = LightSensor(27)
+start = time.time();
+while (time.time() - start) > 10:
+    bright = lsr.value;
+    print(str(bright))
+    if bright == 1:
+        led.on()
+    else:
+        led.off()
