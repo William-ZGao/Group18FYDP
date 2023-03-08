dcc4e776cddbb31c0b77fddb2eeda19078519b7e
Zi Zhang <z785zhan@uwaterloo.ca>
2023-03-08 15:22:06 -0500

    digital input

--- test1.py
+++ test1.py
@@ -1,5 +1,5 @@
 import time
-from gpiozero import LED, LightSensor
+from gpiozero import LED, DigitalInputDevice
 
 led = LED(4)
 led.on()
@@ -8,12 +8,12 @@ time.sleep(1)
 led.off()
 print("LED off!")
 
-lsr = LightSensor(27)
+lsr = DigitalInputDevice(27)
 start = time.time()
-while (time.time() - start) > 10:
-    bright = lsr.value
-    print(str(bright))
-    if bright == 1:
+while (time.time() - start) < 10:
+    print(str(lsr.value))
+    if lsr.value > 0:
+        print("light!")
         led.on()
     else:
         led.off()
